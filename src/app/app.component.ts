1343f9f38e9782bedfeca6575d444a45edba970a
lucasklaassen <lucasklaassen1@gmail.com>
2018-08-14 19:49:32 -0700

    Create promise examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  successfulPromise: Promise<string>;
+  errorPromise: Promise<string>;
+
+  ngOnInit(): void {
+    // Create promise that resolves successfully
+    this.successfulPromise = new Promise((resolve, _) => {
+      setTimeout(() => {
+        resolve('promise has resolved');
+      }, 5000);
+    });
+    this.successfulPromise.then((result) => {
+      this.title = result;
+    });
+
+
+    // Create promise that fails and is rejected
+    this.errorPromise = new Promise((_, reject) => {
+      setTimeout(() => {
+        reject('promise has failed :(');
+      }, 5000);
+    });
+    this.successfulPromise.then((result) => {
+      // This should never hit
+      this.title = result;
+    }, (error) => {
+      console.error(error);
+    });
+  }
 }
